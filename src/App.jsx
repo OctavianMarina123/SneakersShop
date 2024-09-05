cb4a7220574f3321408c4acd889a232e2d196e52
Octa Marina <octamarina@Octas-Mac-mini.local>
2024-09-05 15:49:47 +0300

    Created the first iteration of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <main>
+            Navbar
+            <section className="xl:padding-1 wide:padding-r padding-b">
+                Hero
+            </section>
+            <section className="padding">
+                PopularProducts
+            </section>
+            <section className="padding">
+                SuperQuality
+            </section>
+            <section className="padding-x py-10">
+                Services
+            </section>
+            <section className="padding-x padding-t pb-8">
+                Footer
+            </section>
+        </main>
     )
 }
